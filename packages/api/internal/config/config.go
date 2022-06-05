3a8c5b800d84731c9c96b065faa9e9c70c9b9d90
Mark Cooper <cooperm1700@hotmail.co.uk>
2022-06-05 23:47:10 +0100

    Change hyphen to underscore

--- packages/api/internal/config/config.go
+++ packages/api/internal/config/config.go
@@ -4,7 +4,7 @@ import "github.com/kelseyhightower/envconfig"
 
 type Config struct {
 	Port int    `envconfig:"PORT" default:"8080"`
-	DSN  string `envconfig:"DSN" default:"postgresql://user:password@localhost:5432/game-recommender?sslmode=disable"`
+	DSN  string `envconfig:"DSN" default:"postgresql://user:password@localhost:5432/game_recommender?sslmode=disable"`
 }
 
 func Load() (*Config, error) {
