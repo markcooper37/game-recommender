6cf98f3c800b48a0a82e48fdaa0280204d5132a1
Mark Cooper <cooperm1700@hotmail.co.uk>
2022-06-09 14:55:43 +0100

    Remove unused variable

--- packages/api/internal/server/handlers.go
+++ packages/api/internal/server/handlers.go
@@ -2,11 +2,7 @@ package server
 
 import (
 	"net/http"
-
-	"github.com/markcooper37/game-recommender/packages/api/internal/models"
 )
-
-var Games = []models.Game{{Name: "Uno"}, {Name: "Monopoly"}, {Name: "Twister"}}
 
 func handleHome() http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
