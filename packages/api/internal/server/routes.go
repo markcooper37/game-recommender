511b9cad0cd044c8d15da6b93017aad0c2b64b2d
Mark Cooper <cooperm1700@hotmail.co.uk>
2022-06-16 22:42:12 +0100

    Remove concat file

--- packages/api/internal/server/routes.go
+++ packages/api/internal/server/routes.go
@@ -1,9 +1,13 @@
 package server
 
 import (
+	"bytes"
 	"embed"
 	"fmt"
+	"io"
+	"io/fs"
 	"net/http"
+	"strings"
 
 	upload "github.com/eko/graphql-go-upload"
 	"github.com/go-chi/chi"
@@ -53,4 +57,51 @@ func MakeGraphQLSchema(db *gorm.DB) (*graphql.Schema, error) {
 	rootResolver := resolvers.NewRootResolver(db)
 
 	return graphql.ParseSchema(schemaDefinition, rootResolver, opts...)
+}
+
+func ConcatGraphQLSchemaFiles(fsys fs.FS) (string, error) {
+	var buf bytes.Buffer
+
+	err := fs.WalkDir(fsys, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return fmt.Errorf("walking dir: %w", err)
+		}
+
+		fileSuffix := fmt.Sprintf(".%s", "graphql")
+		if !strings.HasSuffix(path, fileSuffix) {
+			return nil
+		}
+
+		f, err := fsys.Open(path)
+		if err != nil {
+			return fmt.Errorf("opening file %q: %w", path, err)
+		}
+		defer f.Close()
+
+		fileInfo, err := f.Stat()
+		if err != nil {
+			return fmt.Errorf("getting stats for file %q: %w", path, err)
+		}
+
+		if fileInfo.IsDir() {
+			return nil
+		}
+
+		_, err = io.Copy(&buf, f)
+		if err != nil {
+			return fmt.Errorf("writing %q bytes to buffer: %w", path, err)
+		}
+
+		_, err = fmt.Fprint(&buf, "\n")
+		if err != nil {
+			return fmt.Errorf("writing newline to buffer: %w", err)
+		}
+
+		return nil
+	})
+	if err != nil {
+		return buf.String(), fmt.Errorf("walking content directory: %w", err)
+	}
+
+	return buf.String(), nil
 }
